73888b5e50f7a48294193808d33330cb71824a13
Gaurav Kumar <gk9596355299@gmail.com>
2025-02-11 14:49:12 +0530

    Updated feature X and fixed bug Y

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -24,7 +24,7 @@ algorithm optimization.
                     </p>
 
                     <a download="" href={CV} className="button button--flex">
-                        Grv_Resume
+                        Gaurav_Resume
                         <svg
                             className="button__icon"
                             xmlns="http://www.w3.org/2000/svg"
